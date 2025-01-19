715d03c6e11cceaac7bac148c979c9bb341fa674
Paul <paul.cem@gmail.com>
2025-01-19 13:47:19 +0000

    fix error

--- my_project/settings.py
+++ my_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^ig-y@65&en5#=5$va3tf@0_32yyda6-0&dnkrjqjl36$&ziak'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-^ig-y@65&en5#=5$va3tf@0_32yyda6-0&dnkrjqjl36$&ziak')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '.herokuapp.com').split(',')
 
 
 # Application definition
