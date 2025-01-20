93926a36f921380cf580eb6d17e153f0de2beb7b
paulabes <paul.cem@gmail.com>
2025-01-20 10:48:30 +0000

    update views name

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world!")
+
+
+def my_blog(request):
+    return HttpResponse("Hello, Blog!")
 
